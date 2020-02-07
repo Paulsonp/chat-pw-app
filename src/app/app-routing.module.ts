e08f3811e0e5637c3a18933a08ca5be2bf9e2fbe
Paulsonps <paulsonps@neoito.com>
2020-02-07 12:36:46 +0530

    api service and module files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
+import { LayoutContainerComponent } from "./layout-container/layout-container.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "chat-room" },
+  {
+    path: "",
+    component: LayoutContainerComponent,
+    children: [
+      {
+        path: "chat-room",
+        loadChildren: () => import("./modules/chat-room/chat-room.module").then(mod => mod.ChatRoomModule)
+      },
+      {
+        path: "media-room",
+        loadChildren: () => import("./modules/media-room/media-room.module").then(mod => mod.MediaRoomModule)
+      }
+    ]
+  },
+  { path: "**", redirectTo: "chat-room" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import {
+  MatToolbarModule,
+  MatCardModule,
+  MatButtonModule
+} from "@angular/material";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { LayoutContainerComponent } from "./layout-container/layout-container.component";
+import { ServiceWorkerModule } from "@angular/service-worker";
+import { environment } from "../environments/environment";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, LayoutContainerComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ServiceWorkerModule.register("ngsw-worker.js", {
+      enabled: environment.production
+    }),
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
