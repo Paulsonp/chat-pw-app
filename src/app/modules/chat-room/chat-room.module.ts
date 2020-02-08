845e4dddbb7da919799b2345c7f4eaf4a580a331
Paulsonps <paulsonps@neoito.com>
2020-02-08 22:01:41 +0530

    ui changes done

--- src/app/modules/chat-room/chat-room.module.ts
+++ src/app/modules/chat-room/chat-room.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { FlexLayoutModule } from "@angular/flex-layout";
+
 import { ChatLayoutComponent } from "./container/chat-layout/chat-layout.component";
 import { ChatRoomRouting } from "./chat-room.routing";
 
 @NgModule({
-  imports: [CommonModule, ChatRoomRouting],
+  imports: [CommonModule, FlexLayoutModule, ChatRoomRouting],
   declarations: [ChatLayoutComponent]
 })
 export class ChatRoomModule {}
